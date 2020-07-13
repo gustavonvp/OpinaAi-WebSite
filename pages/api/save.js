d9d58bff19a90d25a4ee5b0c37e5e63fd5e0fc52
gustavonvp <gustavonvp@github.com>
2020-07-13 01:45:22 -0300

    3 commit

--- pages/api/save.js
+++ pages/api/save.js
@@ -2,7 +2,7 @@ import  { GoogleSpreadsheet } from 'google-spreadsheet'
 
 import moment from 'moment'
 
-import  credentials from '../../credentials.json'
+
 
 const doc = new GoogleSpreadsheet(process.env.SHEET_DOC_ID)
 
@@ -14,7 +14,10 @@ const genCupom = () => {
 export default async(req,res) => {
 
     try{ 
-        await doc.useServiceAccountAuth(credentials)
+        await doc.useServiceAccountAuth({
+            client_email: process.env.SHEET_CLIENT_EMAIL,
+            private_key:process.env.SHEET_PRIVATE_KEY
+        })
         await doc.loadInfo()
         const sheet = doc.sheetsByIndex[1]
         const data = JSON.parse(req.body)
