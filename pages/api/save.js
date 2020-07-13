402b8d4ceb4ab066c85aab37516cbf31221f0aa5
gustavonvp <gustavonvp@github.com>
2020-07-13 03:11:52 -0300

    commit

--- pages/api/save.js
+++ pages/api/save.js
@@ -1,6 +1,6 @@
 import  { GoogleSpreadsheet } from 'google-spreadsheet'
 
-import { fromBase64 } from '../../utils/base64'
+import fromBase64  from  '../../utils/base64'
 
 import moment from 'moment'
 
