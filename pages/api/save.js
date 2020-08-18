246a346077fc0feae5ffa15f305719b8960613a0
gustavonvp <54008916+gustavonvp@users.noreply.github.com>
2020-08-18 15:16:59 -0300

    encode64

--- pages/api/save.js
+++ pages/api/save.js
@@ -10,7 +10,10 @@ const code = parseInt(moment().format('YYMMDDHHmmssSSS')).toString(16).toUpperCa
 return code.substr(0,4) + '-' + code.substr(4,4) + '-' + code.substr(8,4)
 }
 
-
+const fromBase64 = value =>{
+    const buff = new Buffer.from(value, 'base64');
+    return buff.toString('ascii')
+}
 
 export default async(req,res) => {
 
@@ -18,7 +21,7 @@ export default async(req,res) => {
 
         await doc.useServiceAccountAuth({
             client_email:process.env.SHEET_CLIENT_EMAIL,
-            private_key: process.env.SHEET_PRIVATE_KEY
+            private_key: fromBase64(process.env.SHEET_PRIVATE_KEY)
         })
 await doc.loadInfo()
 const sheet = doc.sheetsByIndex[1];
